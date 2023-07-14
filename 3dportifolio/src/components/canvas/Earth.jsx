5f6154cdb118f278aec1e074f53e23bb8e21a113
joroinnoroff <oinojorgen@gmail.com>
2023-07-15 00:03:34 +0200

    reactportifolio
    Javascript mastery React Portifolio Course - https://www.youtube.com/watch?v=tllZWCQZ9_0&t=33310s

---
+++ 3dportifolio/src/components/canvas/Earth.jsx
@@ -0,0 +1,56 @@
+import { Suspense } from "react"
+import { Canvas } from "@react-three/fiber"
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei"
+
+import CanvasLoader from '../Loader';
+import { PointLight } from "three";
+
+
+
+
+
+const Earth = () => {
+
+  const earth = useGLTF('./earth_hologram/scene.gltf')
+  return (
+   <primitive 
+   object={earth.scene}
+   scale={1}
+   position-y={0}
+   rotation-y={0}
+   />
+  )
+}
+
+const EarthCanvas = () => {
+  return(
+    <Canvas
+    shadows
+    frameloop="demand"
+    gl={{ preserveDrawingBuffer: true }}
+    camera={{
+      fov:35,
+      near: 0.1,
+      far: 200,
+      position: [-4, 3, 6]
+     }}
+    
+    >
+     
+      <Suspense
+      fallback={<CanvasLoader />}>
+        <OrbitControls 
+        
+        autoRotate
+        enableZoom={false}
+        maxPolarAngle={Math.PI / 2}
+        minPolarAngle={Math.PI / 2} 
+          />
+
+          <Earth />
+      </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas
